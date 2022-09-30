8d57e3b086b917fdddd769f8db798512aa229bfb
ZouhairEM <highaeonkraktex@gmail.com>
2022-09-30 20:39:55 +0200

    inital commit with scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <form>
+      <input
+        type="text"
+        name="dino"
+        className="myDino"
+        aria-describedby="dino_name_info"
+      />
+    </form>
+  );
+}
+
+export default App;
